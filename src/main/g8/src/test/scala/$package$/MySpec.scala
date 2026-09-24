8bfd35d9e8bed2d7a80dc4ab9489b3185db11f24
Roy Andrew <hello@royandrew.dev>
2026-09-24 21:35:09 +0100

    Update Scala and sbt versions; enhance MySpec for Playwright testing

--- src/main/g8/src/test/scala/$package$/MySpec.scala
+++ src/main/g8/src/test/scala/$package$/MySpec.scala
@@ -1,11 +1,11 @@
-package $package$
+package $package$.playwright
 
 import com.microsoft.playwright.*;
 
 class MySpec extends BaseChromiumSpec {
   "playwright" should "succeed" in {   
     val page = browser.newPage();
-    page.navigate("http://playwright.dev");
+    page.navigate("https://playwright.dev");
     page.title shouldBe "Fast and reliable end-to-end testing for modern web apps | Playwright"
   }
 }
